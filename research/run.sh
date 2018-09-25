305cdef8b626a1ad465d16f4e2760288ead1f2e8
huobaoxiaopipi <43368353+huobaoxiaopipi@users.noreply.github.com>
2018-09-25 15:45:45 +0800

    Update run.sh

--- research/run.sh
+++ research/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+# 查找脚本所在路径，并进入
+#DIR="$( cd "$( dirname "$0"  )" && pwd  )"
+DIR=$PWD
+cd $DIR
+echo current dir is $PWD
 
 
 # 设置目录，避免module找不到的问题
@@ -24,11 +28,7 @@ pipeline_config_path=$output_dir/$config
 # 因为dataset里面的东西是不允许修改的，所以这里要把config文件复制一份到输出目录
 cp $dataset_dir/$config $pipeline_config_path
 
-# 查找脚本所在路径，并进入
-#DIR="$( cd "$( dirname "$0"  )" && pwd  )"
-DIR=$PWD
-cd $DIR
-echo current dir is $PWD
+
 
 for i in {0..4}  # for循环中的代码执行5此，这里的左右边界都包含，也就是一共训练500个step，每100step验证一次
 do
